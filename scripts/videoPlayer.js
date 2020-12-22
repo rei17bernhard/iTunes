2b75d2d0daf7a47c7ee9ea82d08942a541778380
Olga Grigoreva <nordavindens.sjel@gmail.com>
2020-12-22 11:54:52 +0300

    Update videoPlayer.js

--- scripts/videoPlayer.js
+++ scripts/videoPlayer.js
@@ -13,11 +13,50 @@ export const videoPlayerInit = () => {
     const videoProgress = document.querySelector('.video-progress');
     const videoTimeTotal = document.querySelector('.video-time__total');
 
-    videoPlayer.addEventListener('click', () => {
+    const toggleIcon = () => {
+        if(videoPlayer.paused){
+            videoButtonPlay.classList.remove('fa-pause');
+            videoButtonPlay.classList.add('fa-play');
+        } else {
+            videoButtonPlay.classList.add('fa-pause');
+            videoButtonPlay.classList.remove('fa-play');
+        }
+    };
+
+    const togglePlay = () => {
         if(videoPlayer.paused){
             videoPlayer.play();
         } else {
             videoPlayer.pause();
         }
-    })
-}
+    };
+
+    const stopPlay = () => {
+        videoPlayer.pause();
+        videoPlayer.currentTime = 0;
+    };
+
+    const addZero = n => n < 10 ? '0' + n : n;
+
+    videoPlayer.addEventListener('click', togglePlay);
+    videoButtonPlay.addEventListener('click', togglePlay);
+
+    videoPlayer.addEventListener('play', togglePlay);
+    videoPlayer.addEventListener('pause', togglePlay);
+
+    videoButtonStop.addEventListener('click', stopPlay);
+
+    videoPlayer.addEventListener('timeupdate', () => {
+        const currentTime = videoPlayer.currentTime;
+        const duration = videoPlayer.duration;
+
+        let minutePassed = Math.floor(currentTime / 60);
+        let secondsPassed = Math.floor(currentTime % 60);
+
+        let minuteTotal = Math.floor(duration / 60);
+        let secondsTotal = Math.floor(duration % 60);
+
+        videoTimePassed.textContent = addZero(minutePassed) + ':' + addZero(secondsPassed);
+        videoTimeTotal.textContent = addZero(minuteTotal) + ':' + addZero(secondsTotal);
+    });
+};
